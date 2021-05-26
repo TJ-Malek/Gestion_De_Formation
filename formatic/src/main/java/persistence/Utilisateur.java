2a978e453f872eaba92a2fe47f574b2f9ffc9782
MalekTajine <tajinemalek@gmail.com>
2021-05-26 15:27:16 +0200

    utilisateur

---
+++ formatic/src/main/java/persistence/Utilisateur.java
@@ -0,0 +1,53 @@
+package persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name="Utilisateur")
+public class Utilisateur {
+@Id
+Long id;
+String nom;
+String prenom;
+String role;
+String email;
+String mdp;
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public String getPrenom() {
+	return prenom;
+}
+public void setPrenom(String prenom) {
+	this.prenom = prenom;
+}
+public String getRole() {
+	return role;
+}
+public void setRole(String role) {
+	this.role = role;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getMdp() {
+	return mdp;
+}
+public void setMdp(String mdp) {
+	this.mdp = mdp;
+}
+
+
+}
