1db0c1ccef5bee44d2324691752355e41f12b597
MalekTajine <tajinemalek@gmail.com>
2021-05-26 15:41:13 +0200

    cours

---
+++ formatic/src/main/java/persistence/Cours.java
@@ -0,0 +1,46 @@
+package persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name="Cours")
+public class Cours {
+	@Id
+	Long id;
+	String designation;
+	String image;
+	String date_ajout;
+	Boolean etat;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getDate_ajout() {
+		return date_ajout;
+	}
+	public void setDate_ajout(String date_ajout) {
+		this.date_ajout = date_ajout;
+	}
+	public Boolean getEtat() {
+		return etat;
+	}
+	public void setEtat(Boolean etat) {
+		this.etat = etat;
+	}
+	
+	
+}
