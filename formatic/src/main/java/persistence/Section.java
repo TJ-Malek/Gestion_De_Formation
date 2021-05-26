5d87692523103d211337b219e43cd6987b1bc7ab
MalekTajine <tajinemalek@gmail.com>
2021-05-26 15:47:06 +0200

    section

---
+++ formatic/src/main/java/persistence/Section.java
@@ -0,0 +1,60 @@
+package persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name="Section")
+public class Section {
+	@Id
+	Long id;
+	String designation;
+	Boolean etat;
+	String image;
+	String titre;
+	String contenu;
+    String date_ajout;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public Boolean getEtat() {
+		return etat;
+	}
+	public void setEtat(Boolean etat) {
+		this.etat = etat;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getContenu() {
+		return contenu;
+	}
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+	public String getDate_ajout() {
+		return date_ajout;
+	}
+	public void setDate_ajout(String date_ajout) {
+		this.date_ajout = date_ajout;
+	}
+    
+    
+}
