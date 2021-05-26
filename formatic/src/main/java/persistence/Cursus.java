e4a74bd6947edfa18e193d46d8812cd89c84ba10
MalekTajine <tajinemalek@gmail.com>
2021-05-26 15:37:44 +0200

    cursus

---
+++ formatic/src/main/java/persistence/Cursus.java
@@ -0,0 +1,60 @@
+package persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name="Cursus")
+public class Cursus {
+	@Id
+	Long id;
+	String designation;
+	String contenu;
+	String pdf;
+	String image;
+	int volume_horaire;
+	Boolean etat;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	public String getContenu() {
+		return contenu;
+	}
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+	public String getPdf() {
+		return pdf;
+	}
+	public void setPdf(String pdf) {
+		this.pdf = pdf;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public int getVolume_horaire() {
+		return volume_horaire;
+	}
+	public void setVolume_horaire(int volume_horaire) {
+		this.volume_horaire = volume_horaire;
+	}
+	public Boolean getEtat() {
+		return etat;
+	}
+	public void setEtat(Boolean etat) {
+		this.etat = etat;
+	}
+	
+	
+}
