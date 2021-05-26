bc14f111b665a8118bbc01289c99e459887c4b77
MalekTajine <tajinemalek@gmail.com>
2021-05-26 15:22:32 +0200

    code

---
+++ formatic/src/main/java/com/example/formatic/FormaticApplication.java
@@ -0,0 +1,13 @@
+package com.example.formatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormaticApplication.class, args);
+	}
+
+}
---
+++ formatic/src/test/java/com/example/formatic/FormaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.formatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
