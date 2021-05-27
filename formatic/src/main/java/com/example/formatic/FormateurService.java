064009437bf78737d94ff34ef6f2d1ff5bb9546f
Malek <tajinemalek@gmail.com>
2021-05-27 17:19:12 +0200

    correction formateur service

--- formatic/src/main/java/com/example/formatic/FormateurService.java
+++ formatic/src/main/java/com/example/formatic/FormateurService.java
@@ -29,70 +29,70 @@ public class FormateurService {
 			return repoUtilisateur.findUtilisateurByEmail(user.getEmail());
 			
 		}
-		// recupere etat formateur
-		
-			public Boolean getEtatFormateur(Formateur formateur) {
-				return repo.getEtat(formateur.getId()).getEtat();
-			}
-		
-		// change etat formateur
-		
-			public Formateur setEtatFormateur(Formateur formateur) {
-				Boolean etat = getEtatFormateur(formateur);
-				return repo.setEtat(!etat,formateur.getId());
-			}
-			
-		// liste de tous les formateurs
-		public List<Formateur> listAll() {
-			 List<Formateur> listFormateur= new ArrayList<>();
-			 for (Formateur l : repo.findAll()) {
-				 listFormateur.add(get(l));
-		      }
-			 return listFormateur;
+	// recupere etat formateur
+	
+		public Boolean getEtatFormateur(Formateur formateur) {
+			return repo.getEtat(formateur.getId()).getEtat();
 		}
+	
+	// change etat formateur
+	
+	public Formateur setEtatFormateur(Formateur formateur) {
+		Boolean etat = getEtatFormateur(formateur);
+		return repo.setEtat(!etat,formateur.getId());
+	}
+	
+	// liste de tous les formateurs
+	public List<Formateur> listAll() {
+		 List<Formateur> listFormateur= new ArrayList<>();
+		 for (Formateur l : repo.findAll()) {
+			 listFormateur.add(get(l));
+	      }
+		 return listFormateur;
+	}
+	
+	// enregistre le formateur s'il n'existe pas dans la BD
+	public Boolean save(Formateur formateur) {
+		if(findFormateur(formateur)==null) {
+				repo.save(formateur);
+				return true;
+	}
+		else {
+				return false;
+		}
+	}
+	
+	// modification formateur
+	public void update(Formateur formateur) {
+		Utilisateur user = new Utilisateur();
+		user.setId(formateur.getId());
+		user.setEmail(formateur.getEmail());
+		user.setMdp(formateur.getMdp());
+		user.setNom(formateur.getNom());
+		user.setPrenom(formateur.getPrenom());
+		user.setRole(formateur.getRole());
+		serviceUser.update(user);
+		repo.save(formateur);
+	}
+	
+	// recupere formateur par son id 
+	public Formateur get(Formateur formateur) {
+		Utilisateur user = new Utilisateur();
+		user.setId(formateur.getId());
 		
-		// enregistre le formateur s'il n'existe pas dans la BD
-		public Boolean save(Formateur formateur) {
-			if(findFormateur(formateur)==null) {
-					repo.save(formateur);
-					return true;
-		}
-			else {
-					return false;
-			}
-		}
+		Utilisateur u = serviceUser.get(user);
 		
-		// modification formateur
-		public void update(Formateur formateur) {
-			Utilisateur user = new Utilisateur();
-			user.setId(formateur.getId());
-			user.setEmail(formateur.getEmail());
-			user.setMdp(formateur.getMdp());
-			user.setNom(formateur.getNom());
-			user.setPrenom(formateur.getPrenom());
-			user.setRole(formateur.getRole());
-			serviceUser.update(user);
-			repo.save(formateur);
-		}
-		
-		// recupere formateur par son id 
-		public Formateur get(Formateur formateur) {
-			Utilisateur user = new Utilisateur();
-			user.setId(formateur.getId());
-			
-			Utilisateur u = serviceUser.get(user);
-			
-			Formateur f = repo.findById(formateur.getId()).get();
-			f.setEmail(u.getEmail());
-			f.setMdp(u.getMdp());
-			f.setNom(u.getNom());
-			f.setPrenom(u.getPrenom());
-			f.setRole(u.getRole());
-			return f;
-		}
-		
-		// supprime formateur
-		public void delete(Formateur formateur) {
-			repo.deleteById(formateur.getId());
-		}
+		Formateur f = repo.findById(formateur.getId()).get();
+		f.setEmail(u.getEmail());
+		f.setMdp(u.getMdp());
+		f.setNom(u.getNom());
+		f.setPrenom(u.getPrenom());
+		f.setRole(u.getRole());
+		return f;
+	}
+	
+	// supprime formateur
+	public void delete(Formateur formateur) {
+		repo.deleteById(formateur.getId());
+	}
 }
