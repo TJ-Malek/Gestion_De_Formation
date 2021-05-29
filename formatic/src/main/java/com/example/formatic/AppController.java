ab0c0be74271ff1191d7422fd9772dde0573f5a2
MalekTajine <tajinemalek@gmail.com>
2021-05-29 19:42:50 +0200

    app controller

--- formatic/src/main/java/com/example/formatic/AppController.java
+++ formatic/src/main/java/com/example/formatic/AppController.java
@@ -3,9 +3,12 @@ package com.example.formatic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 
 
 
@@ -30,15 +33,22 @@ public class AppController {
 	System.out.println("saved = "+t);
 	return "index";
 	}
-	@RequestMapping("/profil")
-	public String viewProfilPage() {
-		return "profil";
+	@RequestMapping("/profil/{id}")
+	public ModelAndView viewProfilPage(@PathVariable(name = "id") Long id) {
+		ModelAndView mav = new ModelAndView("profil");
+		Utilisateur user = new Utilisateur();
+		user.setId(id);
+	    Utilisateur userConnected = serviceUser.get(user);
+		mav.addObject("user", userConnected);
+		
+		return mav;
 	}
 	@RequestMapping(value = "/auth", method = RequestMethod.POST)
 	public String auth(@ModelAttribute("user") Utilisateur user) {
 		Utilisateur find = serviceUser.findUtilisateurByEmailAndMdp(user);
 		if(find!=null) {
-		return "redirect:/profil";
+			
+		return "redirect:/profil/"+find.getId();
 	}
 		else {
 			return "redirect:/";
