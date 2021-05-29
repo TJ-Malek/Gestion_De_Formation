cb81734c6dc7f75659f9e837a0d9055f5c205864
MalekTajine <tajinemalek@gmail.com>
2021-05-29 19:26:42 +0200

    auth page

--- formatic/src/main/java/com/example/formatic/AppController.java
+++ formatic/src/main/java/com/example/formatic/AppController.java
@@ -2,8 +2,12 @@ package com.example.formatic;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 //import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 
 //import persistence.Utilisateur;
 
@@ -30,5 +34,14 @@ public class AppController {
 	public String viewProfilPage() {
 		return "profil";
 	}
-	
+	@RequestMapping(value = "/auth", method = RequestMethod.POST)
+	public String auth(@ModelAttribute("user") Utilisateur user) {
+		Utilisateur find = serviceUser.findUtilisateurByEmailAndMdp(user);
+		if(find!=null) {
+		return "redirect:/profil";
+	}
+		else {
+			return "redirect:/";
+		}
+	}
 }
